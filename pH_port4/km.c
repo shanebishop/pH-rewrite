23040c18198595dec1e902df3c195d7ac7462c97
shane.bishop@outlook.com <shane.bishop@outlook.com>
2017-07-19 14:03:01 -0400

    Updated print statement in process_syscall() (again) to help with debugging cause of kernel paging request failure

--- pH_port4/km.c
+++ pH_port4/km.c
@@ -469,7 +469,7 @@ int process_syscall(long syscall) {
 	//pr_err("%s: process = %p %d\n", DEVICE_NAME, process, process != NULL);
 	//pr_err("%s: binary = %s\n", DEVICE_NAME, process->profile->filename);
 	//pr_err("%s: profile = %p %d\n", DEVICE_NAME, profile, profile != NULL);
-	pr_err("%s: &(process->profile->lock) = %p %d\n", DEVICE_NAME, &(process->profile->lock), process->profile->lock != NULL);
+	pr_err("%s: &(process->profile->lock) = %p %d\n", DEVICE_NAME, &(process->profile->lock));
 	spin_lock(&(profile->lock));
 	//pr_err("%s: &(profile->count) = %p\n", DEVICE_NAME, &(profile->count));
 	profile->count++;
