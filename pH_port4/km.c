650c5fca1be3febd860096d54faf58ed247229f4
shane.bishop@outlook.com <shane.bishop@outlook.com>
2017-07-21 16:41:31 -0400

    Commented out profile->identifer print statement in process_syscall()

--- pH_port4/km.c
+++ pH_port4/km.c
@@ -524,7 +524,7 @@ int process_syscall(long syscall) {
 	//pr_err("%s: process = %p %d\n", DEVICE_NAME, process, process != NULL);
 	//pr_err("%s: binary = %s\n", DEVICE_NAME, process->profile->filename);
 	//pr_err("%s: profile = %p %d\n", DEVICE_NAME, profile, profile != NULL);
-	pr_err("%s: profile->identifer = %d\n", DEVICE_NAME, profile->identifier);
+	//pr_err("%s: profile->identifer = %d\n", DEVICE_NAME, profile->identifier);
 	//pr_err("%s: profile->lock = %p\n", DEVICE_NAME, profile->lock);
 	//spin_lock(profile->lock);
 	//pr_err("%s: &(profile->count) = %p\n", DEVICE_NAME, &(profile->count));
