83a364330f4d4ceb3de08cfbcd6295441876b750
shane.bishop@outlook.com <shane.bishop@outlook.com>
2017-07-31 17:10:05 -0400

    Added some print statements

--- pH_port4/km.c
+++ pH_port4/km.c
@@ -538,7 +538,10 @@ void clean_processes(void) {
 	char* string_to_compare;
 	char* temp;
 	
+	pr_err("%s: In clean_process\n", DEVICE_NAME);
+	
 	for (iterator = pH_task_struct_list; iterator != NULL; iterator = iterator->next) {
+		pr_err("%s: In for\n", DEVICE_NAME);
 		if (iterator->profile == NULL) continue;
 		
 		for (string_to_compare = iterator->profile->filename; 
@@ -550,8 +553,11 @@ void clean_processes(void) {
 		string_to_compare = temp;
 		
 		if (!comm_matches(string_to_compare)) {
+			pr_err("%s: Removing element...\n", DEVICE_NAME);
 			free_pH_task_struct(iterator);
+			pr_err("%s: Got here 1\n", DEVICE_NAME);
 			iterator = iterator->prev;
+			pr_err("%s: Got here 2\n", DEVICE_NAME);
 		}
 	}
 }
@@ -574,7 +580,7 @@ int process_syscall(long syscall) {
 	
 	if (!pH_task_struct_list || pH_task_struct_list == NULL) return 0;
 
-	//pr_err("%s: In process_syscall\n", DEVICE_NAME);
+	pr_err("%s: In process_syscall\n", DEVICE_NAME);
 	
 	// Check to see if a process went out of use
 	//clean_processes(); // Temporarily commented out since the module isn't working at the moment
