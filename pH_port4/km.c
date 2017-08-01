3f85f73eae653277116edb41fc3973070774f568
shane.bishop@outlook.com <shane.bishop@outlook.com>
2017-08-01 14:00:55 -0400

    Minor fixes

--- pH_port4/km.c
+++ pH_port4/km.c
@@ -608,8 +608,8 @@ int process_syscall(long syscall) {
 	pr_err("%s: In process_syscall\n", DEVICE_NAME);
 	
 	// Check to see if a process went out of use
-	clean_processes(); // Temporarily commented out since the module isn't working at the moment
-	return 0; // Temp return
+	//clean_processes(); // Temporarily commented out since the module isn't working at the moment
+	//return 0; // Temp return
 	
 	// Retrieve process
 	process = llist_retrieve_process(pid_vnr(task_tgid(current)));
