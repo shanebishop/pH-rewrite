f4209dbaca557ca65590b615ff9726cc15a70480
shane.bishop@outlook.com <shane.bishop@outlook.com>
2017-08-04 11:16:57 -0400

    Minor fixes

--- pH_port4/km.c
+++ pH_port4/km.c
@@ -975,6 +975,7 @@ static long jsys_execve(const char __user *filename,
 		pr_err("%s: Unable to allocate memory for path_to_binary\n", DEVICE_NAME);
 		goto no_memory;
 	}
+	goto not_monitoring;
 	
 	// Copy memory from userspace to kernel land
 	copy_from_user(path_to_binary, filename, sizeof(char) * 4000);
